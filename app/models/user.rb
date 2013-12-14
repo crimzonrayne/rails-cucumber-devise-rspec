738ce57e25743a9234f54186a147f111d32b2b4e
Andrew H <Heider.Andrew@gmail.com>
2013-12-13 16:20:22 -0800

    added default pages and signup/login forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    validates_presence_of :name
+	validates_uniqueness_of :name, :email, :case_sensitive => false
+
+	private
+
+	def secure_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me)
+	end
 end
